72b9175b74b9401440c79edf77e5721ec2c3f7c0
Marcus Petschlies <marcus.petschlies@web.de>
2021-08-27 15:28:30 +0200

    added R scripts

---
+++ aic.R
@@ -0,0 +1,137 @@
+#####################################################################
+# read data
+#####################################################################
+read_from_file <- function ( f ) {
+  if ( missing ( f ) ) stop ( "[read_from_file] missing parameter values" )
+  message ( "# [read_from_file] reading from file ", f )
+  return ( read.table ( f ) )
+}  # end of read_from_file
+
+#####################################################################
+# stats
+#####################################################################
+stats <- function ( s, t, f, a=T ) {
+
+  if ( missing ( s ) || missing( t ) ) stop ( "[stats] missing argument values" )
+  ms <- mean ( s )
+  es <- sd   ( s )
+
+  if ( !missing(f) ) {
+    cat ( formatC(t, width=-40, format="s"),
+         " & ", 
+         formatC( ms, width=16, digits=4, format="f" ), 
+         " & ", 
+         formatC( es, width=16, digits=4, format="f" ),
+         " \\\\ ", 
+         "\n", file=f, append=a )
+  }
+  return ( invisible( list ( t=t, m=ms, e=es ) ) )
+}  # end of stats
+
+#####################################################################
+# weighted sum of normal distributions for uniroot
+#####################################################################
+wdf <- function(x, w=w, m=m, s=s, a=0 ) {
+  return( sum(w * pnorm(x,mean=m,sd=s) ) - a )
+}
+
+#####################################################################
+# combine RC-factor weighted sample data
+#####################################################################
+aic_stats <- function(workpath="/data/nf211/x/R/" 
+#                        nsample = 1000 , seed 
+                       ) {
+
+#  if ( missing ( seed ) ) stop( "[aic_stats] need seed value" )
+
+  prefix_list <- c( 
+                   "xq-conn/lvl0",
+                   "xq-disc/lvl0",
+                   "xq-disc-strange/lvl0",
+                   "xq-disc-charm/lvl0",
+                   "xg-disc/nstout10/lvl0" 
+  )
+
+  #####################################################################
+  # read data into list v
+  #####################################################################
+  f_list <- c(
+    "fit.cB211.072.64.xq-conn",
+    "fit.cB211.072.64.xq-disc",
+    "fit.cB211.072.64.xq-disc-strange",
+    "fit.cB211.072.64.xq-disc-charm",
+    "fit.cB211.072.64.xg-disc"
+  )
+
+  v <- list()
+
+  for ( i in 1:length(f_list) ) {
+    f <- paste( prefix_list[i], "/", f_list[i], ".aic", sep="") 
+    v[[i]] <- read_from_file ( f )
+  }
+
+  #####################################################################
+  # build the sampling
+  #####################################################################
+
+  for ( i in 1:length(f_list) ) 
+  {
+
+    message( "# [aic_stats] file = ", f_list[i] )
+    d <- v[[i]]
+
+    n <- length(d$V1)
+
+    w <- d$V10 / sum( d$V10 )
+    m <- numeric()
+    s <- numeric()
+
+    #####################################################################
+    #
+    #####################################################################
+#    set.seed ( seed )
+
+    #####################################################################
+    #
+    #####################################################################
+    output_file <- paste( f_list[i], ".aic_stats", sep="" )
+    cat( "# ", date(), "\n", file=output_file, append=F)
+#    cat( "# nsample  ", nsample , "\n", append=T, file=output_file )
+
+    #####################################################################
+    #
+    #####################################################################
+    for ( k in 1:dim(d)[1] )
+    {
+      lvl <- d[k,1]
+      if ( lvl == 0 ) {
+        m[k] <- d[k,15]
+        s[k] <- d[k,16]
+      }
+    }  # end of loop file entries
+  
+    # return( list(m=m,s=s,w=w))
+    lower <- min ( m ) - 5*max ( s )
+    upper <- max ( m ) + 5*max ( s )
+
+    u_m <- uniroot( wdf, lower=lower, upper=upper , check.conv = T, tol = 1.e-12, maxiter = 100000, a=0.50, s=s, w=w, m=m )
+    u_l <- uniroot( wdf, lower=lower, upper=upper , check.conv = T, tol = 1.e-12, maxiter = 100000, a=0.16, s=s, w=w, m=m )
+    u_u <- uniroot( wdf, lower=lower, upper=upper , check.conv = T, tol = 1.e-12, maxiter = 100000, a=0.84, s=s, w=w, m=m )
+
+
+    # return ( list(h=h, l=idxl, u=idxu, m=idxm , a=a) )
+
+    cat ( "# convergence ", u_m$f.root, u_u$f.root, u_l$f.root, "\n", file=output_file, append=TRUE ) 
+    cat( 
+        formatC( u_m$root, width=6, digits=4, format="f" ),
+        formatC( u_m$root - u_l$root, width=6, digits=4, format="f" ),
+        formatC( u_u$root - u_m$root, width=6, digits=4, format="f" ),
+        "\n", file=output_file, append=T )
+
+
+  }  # end of loop on files
+
+
+  return ( NULL )
+    
+}  # end of aic_stats
---
+++ renormalize.R
@@ -0,0 +1,144 @@
+# non-singlet
+Z_qq1  <- c(1.151, 0.004 )
+Z_qq2  <- c(1.160, 0.003 )
+
+# singlet
+Z_qqs1 <- c(1.161, 0.024 )
+Z_qqs2 <- c(1.163, 0.012 )
+
+# gluon
+Z_gg2 <- c(1.08, 0.17 )
+
+# mixing coefficients
+Z_gq1 <- c(  0.232, 0 )
+Z_gq2 <- c(  0.083, 0 )
+Z_qg1 <- c( -0.027, 0 )
+
+#Z_gq1 <- c( 0, 0 )
+#Z_gq2 <- c( 0, 0 )
+#Z_qg1 <- c( 0, 0 )
+
+#Z_gq1 <- c(  0.232, 0.232 )
+#Z_gq2 <- c(  0.083, 0.083 )
+#Z_qg1 <- c( -0.027, 0.027 )
+
+#####################################################################
+# read data
+#####################################################################
+read_col_from_file <- function ( f, c ) {
+  if ( missing ( f ) || missing ( c ) ) stop ( "[read_col_from_file] missing parameter values" )
+  message ( "# [read_col_from_file] reading col ", c, " from file ", f )
+  return ( read.table ( f )[,c] )
+}  # end of read_col_from_file
+
+#####################################################################
+# stats
+#####################################################################
+stats <- function ( s, t, f, a=T ) {
+
+  if ( missing ( s ) || missing( t ) ) stop ( "[stats] missing argument values" )
+  ms <- mean ( s )
+  es <- sd   ( s )
+
+  if ( !missing(f) ) {
+    cat ( formatC(t, width=-40, format="s"),
+         " & ", 
+         formatC( ms, width=16, digits=4, format="f" ), 
+         " & ", 
+         formatC( es, width=16, digits=4, format="f" ),
+         " \\\\ ", 
+         "\n", file=f, append=a )
+  }
+  return ( invisible( list ( t=t, m=ms, e=es ) ) )
+}  # end of stats
+
+#####################################################################
+# combine RC-factor weighted sample data
+#####################################################################
+combine_renormalized <- function(workpath="/data/nf211/x/R/" ) {
+
+  col <- 3
+  nf <- 2+1+1
+
+  xL_f <- "xq-conn/lvl0/aic/fit.cB211.072.64.xq-conn.ts56_64_72_80.tc4.tf32_64.bs"
+
+  xl_f <- "xq-disc/lvl0/aic/fit.cB211.072.64.xq-disc.ts12_16_20_24_28_32.tc4.tf32_64.bs"
+
+  xs_f <- "xq-disc-strange/lvl0/aic/fit.cB211.072.64.xq-disc-strange.ts12_16_20_24_28_32.tc4.tf32_64.bs"
+
+  xc_f <- "xq-disc-charm/lvl0/aic/fit.cB211.072.64.xq-disc-charm.ts12_16_20_24_28_32.tc4.tf32_64.bs"
+
+  xg_f <- "xg-disc/nstout10/lvl0/aic/fit.cB211.072.64.xg-disc.ts20_24_28_32.tc7.tf32_64.bs"
+
+  xL_s <- read_col_from_file ( xL_f, col ) / 2 # divide by 2 for single light quark contribution, L <- ( U + D ) / 2
+  xl_s <- read_col_from_file ( xl_f, col ) / 2 # divide by 2 for single light quark contribution, l <- ( u + d ) / 2
+  xs_s <- read_col_from_file ( xs_f, col )
+  xc_s <- read_col_from_file ( xc_f, col )
+  xg_s <- read_col_from_file ( xg_f, col )
+
+  ns <- length ( xL_s )
+  if ( length ( xl_s ) != ns || length ( xs_s ) != ns || length ( xc_s ) != ns || length ( xg_s ) != ns ) stop ( "different sample sizes" )
+
+  #####################################################################
+  # difference singlet - non-singlet
+  #####################################################################
+
+  dZ_qq1 <- Z_qqs1 - Z_qq1
+  dZ_qq2 <- Z_qqs2 - Z_qq2
+
+  #####################################################################
+  # RC factor samples
+  #####################################################################
+  sZ_qq1  <- rnorm( n = ns, mean = Z_qq1[1], sd = Z_qq1[2] )
+
+  sZ_qq2  <- rnorm( n = ns, mean = Z_qq2[1], sd = Z_qq2[2] )
+
+  sdZ_qq1 <- rnorm( n = ns, mean = dZ_qq1[1], sd = dZ_qq1[2] )
+ 
+  sdZ_qq2 <- rnorm( n = ns, mean = dZ_qq2[1], sd = dZ_qq2[2] )
+ 
+  sZ_gg2  <- rnorm( n = ns, mean = Z_gg2[1], sd = Z_gg2[2] )
+
+  sZ_gq1  <- rnorm( n = ns, mean = Z_gq1[1], sd = Z_gq1[2] )
+
+  sZ_gq2  <- rnorm( n = ns, mean = Z_gq2[1], sd = Z_gq2[2] )
+
+  sZ_qg1  <- rnorm( n = ns, mean = Z_qg1[1], sd = Z_qg1[2] )
+
+  #####################################################################
+  # combine
+  #####################################################################
+
+  # light
+  rxl_s <- ( sZ_qq1 * xL_s + sZ_qq2 * xl_s ) + ( sdZ_qq1 * 2 * xL_s + sdZ_qq2 * ( 2 * xl_s + xs_s + xc_s ) ) / nf + sZ_qg1 * xg_s / nf
+  
+  # strange
+  rxs_s <- (                 sZ_qq2 * xs_s ) + ( sdZ_qq1 * 2 * xL_s + sdZ_qq2 * ( 2 * xl_s + xs_s + xc_s ) ) / nf + sZ_qg1 * xg_s / nf
+  
+  # charm
+  rxc_s <- (                 sZ_qq2 * xc_s ) + ( sdZ_qq1 * 2 * xL_s + sdZ_qq2 * ( 2 * xl_s + xs_s + xc_s ) ) / nf + sZ_qg1 * xg_s / nf
+  
+  # gluon
+  rxg_s <- sZ_gq1 * 2 * xL_s + sZ_gq2 * ( 2 * xl_s  + xs_s + xc_s )  + sZ_gg2 * xg_s
+
+  #####################################################################
+  # mean and variance
+  #####################################################################
+  output_file <- "xq-renormalized.tab"
+  cat( "# ", date(), "\n", file=output_file, append=F )
+
+  stats ( s = rxl_s, t = "l", f = output_file )
+
+  stats ( s = rxs_s, t = "s", f = output_file )
+
+  stats ( s = rxc_s, t = "c", f = output_file )
+
+  stats ( s = rxg_s, t = "g", f = output_file )
+
+  stats ( s = 2*rxl_s+rxs_s, t = "u+d+s", f = output_file )
+
+  stats ( s = 2*rxl_s+rxs_s+rxc_s, t = "u+d+s+c", f = output_file )
+
+  stats ( s = 2*rxl_s+rxs_s+rxc_s+rxg_s, t = "u+d+s+c+g", f = output_file )
+
+}  # end of combine_renormalized
